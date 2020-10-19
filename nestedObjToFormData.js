0d3aadbcc0bfe81484820e4825f5c46fc78a9b1f
Mehmet Emrah TUNÇEL <timemrah@gmail.com>
2020-10-20 00:42:27 +0300

    Update nestedObjToFormData.js
    Unnecessary codes have been cleared.

--- nestedObjToFormData.js
+++ nestedObjToFormData.js
@@ -1,13 +1,13 @@
 function nestedObjToFormData(obj, formData = new FormData()){
 
-    nestedObjToFormData.formData = formData;
+    let delegateFormData = formData;
 
     const createFormData = function(obj, subKeyStr = ''){
         for(let i in obj){
             let value = obj[i];
             let subKeyStrTrans = subKeyStr ? `${subKeyStr}[${i}]` : i;
             if(typeof(value) === 'string' || typeof(value) === 'number'){
-                nestedObjToFormData.formData.append(subKeyStrTrans, value);
+                delegateFormData.append(subKeyStrTrans, value);
             }
             else if(typeof(value) === 'object'){
                 createFormData(value, subKeyStrTrans);
@@ -16,5 +16,5 @@ function nestedObjToFormData(obj, formData = new FormData()){
     }
 
     createFormData(obj);
-    return nestedObjToFormData.formData;
+    return delegateFormData;
 }
